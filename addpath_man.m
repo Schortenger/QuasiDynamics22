a49105790cc823b5b832dc19b1e0b37d87a6f624
Bernardo Aceituno <12869034+baceituno@users.noreply.github.com>
2022-02-07 14:26:34 -0500

    2

---
+++ addpath_man.m
@@ -0,0 +1,11 @@
+% adds all of the manipulation code to the path and gurobi (for MAC OS)
+display('adding code to path');
+current_fldr = pwd;
+cd /Library/gurobi810/mac64/examples/matlab
+cd ../..
+cd matlab
+gurobi_setup
+cd (current_fldr)
+root = fileparts(mfilename('fullpath'));
+addpath(genpath(root))
+addpath('~/Downloads/casadi-osx-matlabR2015a-v3.4.5/')
